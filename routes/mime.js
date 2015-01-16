e342ff2e8237c0675fcae8859ac86ba2779c825e
Guo Yu <i@guoyu.me>
2015-01-16 15:17:14 +0800

    Rewrting routes mime

--- routes/mime.js
+++ routes/mime.js
@@ -2,26 +2,34 @@ var dbModel = require('../models.js');
 var user = dbModel.user;
 var errHandler = require('../lib/error.js');
 
-module.exports = function(req, res) {
-  if (!req.session.uid) return res.redirect('/');
-  user.findById(req.session.uid).populate('client').exec(function(err, doc) {
-    if (!err) {
-      // 判断是否授权trello,引导授权trelo
-      var trelloFlag = doc.trello.token != '' ? true : false;
-      console.log(doc)
+module.exports = route;
 
-      if (trelloFlag) {
-        res.render('mime', {
-          pageTitle: '我的邮差猫',
-          desc: '我的邮差猫个人主页',
-          userInfo: req.session.user,
-          setting: doc.setting,
-          trelloFlag: trelloFlag,
-          user: req.session.user
-        });
-      } else {
-        res.redirect('/mime/update');
-      }
-    } else res.send(errHandler('db'));
-  });
+function route(req, res) {
+  if (!req.session.uid) 
+    return res.redirect('/');
+
+  user
+    .findById(req.session.uid)
+    .populate('client')
+    .exec(response);
+
+  function response(err, doc) {
+    if (err)
+      return res.send(errHandler('db'));
+
+    // 判断是否授权 trello, 引导授权 trello
+    var trelloFlag = doc.trello.token != '';
+
+    if (!trelloFlag)
+      return res.redirect('/mime/update');
+
+    res.render('mime', {
+      pageTitle: '我的邮差猫',
+      desc: '我的邮差猫个人主页',
+      userInfo: req.session.user,
+      setting: doc.setting,
+      trelloFlag: trelloFlag,
+      user: req.session.user
+    });
+  }
 }
